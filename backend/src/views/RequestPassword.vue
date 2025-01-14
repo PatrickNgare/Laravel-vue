d98654e18cfab0a3b102782257152217a87fd717
PatrickNgare <patrickngare@gmail.com>
2025-01-14 16:46:35 +0300

    added the request password view

--- backend/src/views/RequestPassword.vue
+++ backend/src/views/RequestPassword.vue
@@ -16,27 +16,19 @@
   
           <div>
             <div class="flex items-center justify-between">
-              <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
               <div class="text-sm">
-                <router-link :to="{name:'requestPassword'}" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</router-link>
+                <router-link :to="{name:'login'}" class="font-semibold text-indigo-600 hover:text-indigo-500">Remmber  password?</router-link>
 
               </div>
             </div>
-            <div class="mt-2">
-              <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-            </div>
+        
           </div>
   
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send Password</button>
           </div>
         </form>
   
-        <p class="mt-10 text-center text-sm/6 text-gray-500">
-          Not a registered?
-          {{ ' ' }}
-          <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Sign up</a>
-        </p>
       </div>
     </div>
     
