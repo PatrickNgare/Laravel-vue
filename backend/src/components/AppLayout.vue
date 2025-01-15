f8e1e3414debea507f36df57e85255a932d59cc7
PatrickNgare <patrickngare@gmail.com>
2025-01-15 14:49:02 +0300

    added some update on the views

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,12 +1,28 @@
 <template>
-    <div class="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
-          <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <p>part of the layout</p>
+   <div class="min-h-full flex">
 
-          </div>
+<!-- Sidebar -->
+<div class="w-[200px] bg-blue-700">
+  Sidebar
+</div>
+<!-- End Sidebar -->
 
-          <router-view> </router-view>
-        </div>
+<div class="flex-1">
+  <!-- Header -->
+  <header class="h-8 shadow bg-white">Header</header>
+  <!-- End Header -->
+
+  <!-- Content -->
+  <main>
+
+    content
+    <router-view></router-view>
+  </main>
+  <!-- End Content -->
+</div>
+
+</div>
+
 
     </template>
 
