511a4ea57e09fd04ca988f92514d02d1d4211bd7
PatrickNgare <patrickngare@gmail.com>
2025-01-15 09:25:14 +0300

    added the reset password

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -1,48 +1,47 @@
 <template>
-    
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+
+    <div class="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Request Password</h2>
+        <h2 class="mt-10 font-bold tracking-tight text-center text-gray-900 text-2xl/9">Set New Password</h2>
       </div>
-  
+
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
           <div>
-            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <label for="new-password" class="block font-medium text-gray-900 text-sm/6">New Password</label>
             <div class="mt-2">
-              <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-            </div>
-          </div>
-  
-          <div>
-            <div class="flex items-center justify-between">
-              <div class="text-sm">
-                <router-link :to="{name:'login'}" class="font-semibold text-indigo-600 hover:text-indigo-500">Remember  password?</router-link>
 
-              </div>
+              <input type="new-password" name="password" id="password" placeholder="New Password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             </div>
-        
+
           </div>
-  
+          <div class="text-sm">
+                <router-link :to="{name:'login'}" class="font-semibold text-indigo-600 hover:text-indigo-500">Go back home</router-link>
+              </div>
+
+          <label for="password-repeat" class="block font-medium text-gray-900 text-sm/6">Re-enter Password</label>
+          <div class="mt-2">
+              <input type="password-repeat" name="password-repeat" id="password"  placeholder="Re-enter Password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+
           <div>
             <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send Password</button>
           </div>
         </form>
-  
+
       </div>
     </div>
-    
+
   </template>
-  
-  
+
+
   <script>
-  
+
   </script>
-  
-  
+
+
   <style scoped>
-  
-  
-  
+
+
+
   </style>
-  
