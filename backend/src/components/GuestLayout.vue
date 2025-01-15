3b99a576556cd5b3f656c5e6abba1b35e0c71bed
PatrickNgare <patrickngare@gmail.com>
2025-01-15 10:25:00 +0300

    addedthe guest component

--- backend/src/components/GuestLayout.vue
+++ backend/src/components/GuestLayout.vue
@@ -1,13 +1,22 @@
 <template>
+<div class="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 font-bold tracking-tight text-center text-gray-900 text-2xl/9">{{ title }}</h2>
+      </div>
 
+      <slot></slot>
+    </div>
 
 </template>
 
-<script>
+<script setup>
 
-export default{
-    name:"GuestLayout"
-}
+const {title} = defineProps({
+
+    title:String
+
+
+})
 
 </script>
 
