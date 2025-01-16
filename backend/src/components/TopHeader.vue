ae538b20aed77f3dd8e91146fa451fa898dd73d2
PatrickNgare <patrickngare@gmail.com>
2025-01-16 16:52:51 +0300

    added the profile drop down

--- backend/src/components/TopHeader.vue
+++ backend/src/components/TopHeader.vue
@@ -1,21 +1,19 @@
 <template>
-   <header class="h-16 bg-white shadow">
+   <header class="flex items-center justify-between bg-white shadow hd-25">
 
-    <button>
+    <button class="p-4">
 
-        <Bars3Icon/>
+        <Bars3Icon class="w-6"/>
     </button>
-    <div>
-        https://randomuser.me/api/portraits/men/14.jpg
+    <div class="px-4">
 
         <Menu as="div" class="relative inline-block text-left">
         <div>
-          <MenuButton
-            class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-md bg-black/20 hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-          >
-            Options
+          <MenuButton class="flex items-center">
+            <img src="https://randomuser.me/api/portraits/men/14.jpg" class="w-8 mr-2 rounded-full">
+              <small>Patel Smith</small>
             <ChevronDownIcon
-              class="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+              class="w-5 h-5 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
             />
           </MenuButton>
