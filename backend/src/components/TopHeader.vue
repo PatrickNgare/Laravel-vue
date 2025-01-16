21039ad4e7d840a32ea545c21232939d65515ea1
PatrickNgare <patrickngare@gmail.com>
2025-01-16 15:53:45 +0300

    added the top header icons

---
+++ backend/src/components/TopHeader.vue
@@ -0,0 +1,25 @@
+<template>
+   <header class="h-16 bg-white shadow">
+
+    <button>
+
+        <MenuIcon/>
+    </button>
+    <div>
+        https://randomuser.me/api/portraits/men/14.jpg
+    </div>
+   </header>
+
+</template>
+
+        <script setup>
+import { MenuIcon } from '@heroicons/vue/24/outline';
+
+        </script>
+
+
+        <style scoped>
+
+
+
+        </style>
