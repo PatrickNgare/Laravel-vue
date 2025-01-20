29661e47b479fac7c1f95c0f29afe142d7ced542
PatrickNgare <patrickngare@gmail.com>
2025-01-20 16:36:18 +0300

    add all rquired details

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+
+        $credentails= $request->validate([
+         'email'=>['required','email'],
+         'password' =>'required',
+         'remember'=>'boolean',
+
+        ]);
+        $remember=$credentails['remember'] ?? false;
+        unset($credentails['remember']);
+        if (!Auth::attempt($credentails,$remember)){
+            return response([
+             'message'=>'Email or Password is incorrect'
+            ], 422);
+        }
+    }
 }
