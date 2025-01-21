93c6b6e24f59acca9b1773f52e58f51cf7fd3988
PatrickNgare <patrickngare@gmail.com>
2025-01-21 11:07:12 +0300

    added the middleware for admin

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum','admin'])
+->group(function(){
+Route::get('/user',function(Request $request){
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+Route::post('/logout',[\App\Http\Controllers\AuthController::class,'logout']);
+});
+
+
+
+Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
