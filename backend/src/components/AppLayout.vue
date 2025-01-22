31474faac0cf6099701e9ea4e4eb28380a45dde3
PatrickNgare <patrickngare@gmail.com>
2025-01-22 23:13:19 +0300

    added please wait

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -22,7 +22,7 @@
     <!-- v-else Block -->
     <div v-else class="flex items-center justify-center min-h-screen bg-gray-200">
       <svg
-        class="w-16 h-16 text-gray-700 animate-spin"
+        class="w-8 h-8 text-gray-700 animate-spin"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -41,6 +41,7 @@
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
+      <p class="mt-4 text-gray-700">Please wait...</p>
     </div>
   </div>
 </template>
