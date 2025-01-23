54f2a25e7a6a6e7dc0f1e0609b2f64bf4ecb5eb6
PatrickNgare <patrickngare@gmail.com>
2025-01-23 09:25:08 +0300

    added slauggble function

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -41,7 +41,7 @@
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
-      <p class="mt-4 text-gray-700">Please wait...</p>
+      <p class="mt-2 text-gray-700">Please wait...</p>
     </div>
   </div>
 </template>
