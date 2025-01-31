4066fdf9152320c3ff83cbaad938779a07532c87
PatrickNgare <patrickngare@gmail.com>
2025-01-31 08:30:10 +0300

    adding search product

--- backend/src/components/Products.vue
+++ backend/src/components/Products.vue
@@ -122,7 +122,11 @@ onMounted(() => {
 
 function getProducts(url = null) {
 
-    store.dispatch('getProducts',{url});
+    store.dispatch('getProducts',{
+    url,
+     search: search.value,
+     perPage: perPage.value
+});
 }
 
 
