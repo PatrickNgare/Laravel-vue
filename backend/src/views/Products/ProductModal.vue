6b58f9b1d3b1244c3f985ecd865e36aeb067ea16
PatrickNgare <patrickngare@gmail.com>
2025-02-05 12:07:43 +0300

    save the updated prodcuts

--- backend/src/views/Products/ProductModal.vue
+++ backend/src/views/Products/ProductModal.vue
@@ -1,5 +1,5 @@
 <template>
-    
+
     <TransitionRoot appear :show="show" as="template">
       <Dialog as="div" @close="closeModal" class="relative z-10">
         <TransitionChild
@@ -35,22 +35,46 @@
               <header class="py-3 px-4 flex justify-between items-center" >
 
                 <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
-                    
-                   {{  product.id   ? `Updare product: "${props.product.title}"` : 'Create new product' }}
+
+                   {{  product.id   ? `Update product: "${props.product.title}"` : 'Create new product' }}
                 </DialogTitle>
+
+                <button click="closeModal()"
+                class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0,2)]" >
+
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+
+                </button>
+
+
 
               </header>
               <form @submit.prevent="onSubmit">
                 <div class=" bg-white px-4 pt-5 pb-4 ">
-
+                  <CustomInput class="mb-2" v-model="product.title" label="Product Title" />
+                  <CustomInput type="file" class="mb-2"  label="Product Image" @change="file => product.image = file"/>
+                  <CustomInput type="textarea" class="mb-2" v-model="product.description" label="Product Description" />
+                  <CustomInput type="number" class="mb-2" v-model="product.price" label="Price" prepend="$"/>
                 </div>
                 <footer class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex-row-reverse">
-                  
+                  <button type="submit" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm
+                  text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500">
+                    Submit
+                  </button>
+                  <button type="button" click="closeModal()" ref="cancelButtonRef"
+                  class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm
+                  text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500"> >
+
+                    Cancel
+                    </button>
 
                 </footer>
 
               </form>
-            
+
             </DialogPanel>
             </TransitionChild>
           </div>class="absolute left-0 top-0 bg-white right-0 bottom-0 flex items-center justify-center z-50"
@@ -75,7 +99,7 @@
   const loading= ref(false)
 
   const emit = defineEmits(['update:modelValue'])
-  
+
   const props=defineProps({
     modelValue:Boolean,
     product:{
@@ -113,6 +137,26 @@ const product=ref({
   function closeModal() {
     show.value = false
     emit('close')
+  }
+
+  function onSubmit(){
+    loading.value=true
+   if(product.value.id){
+
+    store.dispatch('updateProduct',product.value)
+      .then(Response=>{
+        loading.value=false
+        if(Response.status===200){
+          store.dispatch('getProducts')
+          closeModal()
+        }
+      })
+
+
+   }else{
+    store.dispatch('createProduct',product.value)
+   }
+
   }
 
   </script>
